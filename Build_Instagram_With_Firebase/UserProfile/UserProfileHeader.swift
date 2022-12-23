a5f84c98e250f2c2d41d9e2e7ddf14c6a952f614
yusheng593 <54464265+yusheng593@users.noreply.github.com>
2022-12-23 15:52:35 +0800

    Profile Header and Grid Cells

--- Build_Instagram_With_Firebase/UserProfile/UserProfileHeader.swift
+++ Build_Instagram_With_Firebase/UserProfile/UserProfileHeader.swift
@@ -28,6 +28,72 @@ class UserProfileHeader: UICollectionViewCell {
         return label
     }()
     
+    let postsLabel: UILabel = {
+        let label = UILabel()
+        
+        let attributedText = NSMutableAttributedString(string: "11\n", attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 14)])
+        attributedText.append(NSAttributedString(string: "貼文", attributes: [NSAttributedString.Key.foregroundColor: UIColor.lightGray, NSAttributedString.Key.font: UIFont.systemFont(ofSize: 12)]))
+        label.attributedText = attributedText
+        
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    let followersLabel: UILabel = {
+        let label = UILabel()
+        
+        let attributedText = NSMutableAttributedString(string: "11\n", attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 14)])
+        attributedText.append(NSAttributedString(string: "粉絲", attributes: [NSAttributedString.Key.foregroundColor: UIColor.lightGray, NSAttributedString.Key.font: UIFont.systemFont(ofSize: 12)]))
+        label.attributedText = attributedText
+        
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    let followingLabel: UILabel = {
+        let label = UILabel()
+        
+        let attributedText = NSMutableAttributedString(string: "11\n", attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 14)])
+        attributedText.append(NSAttributedString(string: "追蹤中", attributes: [NSAttributedString.Key.foregroundColor: UIColor.lightGray, NSAttributedString.Key.font: UIFont.systemFont(ofSize: 12)]))
+        label.attributedText = attributedText
+        
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    let editProfileButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("編輯個人檔案", for: .normal)
+        button.setTitleColor(.label, for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.layer.borderWidth = 1
+        button.layer.borderColor = UIColor.lightGray.cgColor
+        button.layer.cornerRadius = 3
+        return button
+    }()
+    
+    let gridButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "grid"), for: .normal)
+        return button
+    }()
+    
+    let listButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "list"), for: .normal)
+        return button
+    }()
+    
+    let bookmarkButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "ribbon"), for: .normal)
+        return button
+    }()
+    
+    //MARK: - init
     override init(frame: CGRect) {
         super.init(frame: frame)
         
@@ -39,6 +105,41 @@ class UserProfileHeader: UICollectionViewCell {
         addSubview(username)
         username.anchor(top: profileImageView.bottomAnchor, left: leftAnchor, bottom: nil, right: rightAnchor, paddingTop: 8, paddingLeft: 16, paddingBottom: 0, paddingRight: 16, width: 0, height: 0)
         
+        setupUserStatsView()
+        setupBottomToolbar()
+        
+        addSubview(editProfileButton)
+        editProfileButton.anchor(top: postsLabel.bottomAnchor, left: postsLabel.leftAnchor, bottom: nil, right: followingLabel.rightAnchor, paddingTop: 2, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 32)
+        
+        
+        
+    }
+    
+    fileprivate func setupBottomToolbar() {
+        let topLineView = UIView()
+        topLineView.backgroundColor = .lightGray
+        let bottomLineView = UIView()
+        bottomLineView.backgroundColor = .lightGray
+        
+        let stakView = UIStackView(arrangedSubviews: [gridButton, listButton, bookmarkButton])
+        stakView.distribution = .fillEqually
+        
+        addSubview(stakView)
+        addSubview(topLineView)
+        addSubview(bottomLineView)
+        
+        stakView.anchor(top: nil, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 50)
+        topLineView.anchor(top: stakView.topAnchor, left: leftAnchor, bottom: nil, right: rightAnchor, paddingTop: 0, paddingLeft: 10, paddingBottom: 0, paddingRight: 0, width: 0, height: 0.5)
+        bottomLineView.anchor(top: nil, left: leftAnchor, bottom: stakView.bottomAnchor, right: rightAnchor, paddingTop: 0, paddingLeft: 10, paddingBottom: 0, paddingRight: 0, width: 0, height: 0.5)
+    }
+    
+    fileprivate func setupUserStatsView() {
+        let stakView = UIStackView(arrangedSubviews: [postsLabel, followersLabel, followingLabel])
+        stakView.distribution = .fillEqually
+        
+        addSubview(stakView)
+        
+        stakView.anchor(top: topAnchor, left: profileImageView.rightAnchor, bottom: nil, right: rightAnchor, paddingTop: 12, paddingLeft: 12, paddingBottom: 0, paddingRight: -12, width: 0, height: 56)
     }
     
     
